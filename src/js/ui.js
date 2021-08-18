6daa685f5a6eef2d4d5da822f7bb22a6e2b6708a
ynlee <ynlee@thecheck.co.kr>
2021-08-18 11:46:05 +0900

    초기 설정 및 레이아웃 작성

---
+++ src/js/ui.js
@@ -0,0 +1,12 @@
+// 하위 브라우저 호환
+document.createElement("article");
+document.createElement("section");
+document.createElement("aside");
+document.createElement("nav");
+document.createElement("header");
+document.createElement("footer");
+document.createElement("main");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+});
